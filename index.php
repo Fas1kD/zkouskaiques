40c369e580b052d05562777e94f44ddbe75aa80e
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2024-02-21 12:41:45 +0100

    První skvělá úprava
    Začínám se svým skvělým kódem

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang ="cs">
+    <head>
+        <meta charset="UTF - 8">
+        <title>Moje stranecka</title>
+        <ling rel = "stylesheet" href = "css/styles.css">
+    </head>
+
+    <body>
+        <header>
+            <h1>Můj skvělý WEB</h1>
+        </header>
+
+        <?php include 'php/menu.php'; ?>
+
+        <main>
+            <p>AHOJ SVĚTE</p>
+
+        </main>
+
+        <?php include 'php/footer.php'; ?>
+        
+    </body>
+
+</html>
